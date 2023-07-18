aeda82f89c9e36eb5b7055cf1c6216290f5d96b0
Kiryl Astapenka <astapenko.1304@gmail.com>
2023-07-18 14:41:42 +0200

    feat: get and add plains

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { getPlain, addPlain } from './src/plain.js';
+
+const app = express();
+const PORT = 3333;
+
+app.use(bodyParser.json());
+
+app.get('/plain', getPlain);
+app.post('/plain', addPlain);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ secret.js
@@ -0,0 +1,1 @@
+export const uri = 'mongodb+srv://airways-api:KdZiMiivJqzLLi7O@cluster0.qbbswik.mongodb.net/?retryWrites=true&w=majority'
